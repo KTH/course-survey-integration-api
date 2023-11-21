fd5f5cd0917c868f9217090416b3b31a569b80ac
Sebastian Ware <sebastian@urbantalk.se>
2023-11-21 10:53:36 +0100

    Housekeeping and clarification

--- shell.nix
+++ shell.nix
@@ -9,6 +9,9 @@ in pkgs.mkShell rec {
     pkgs.azure-cli
     pkgs.nodejs_20
     pkgs.openssl
+    # node-gyp has issues with certain versions of python
+    # https://github.com/nodejs/node-gyp/issues/2219
+    # If this is resolved we can remove this:
     pkgs.python310Full
   ];
 }
