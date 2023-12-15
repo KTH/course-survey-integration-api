f5f526dc6edfa91e456401960693ed2f3ef55b72
Sebastian Ware <sebastian@urbantalk.se>
2023-12-15 16:18:23 +0100

    Revert "Try all in one file"
    This reverts commit fc2afc899b85ac15760a681d0c5bb1c46a999ccc.

--- src/functions/LadokEvents.ts
+++ src/functions/LadokEvents.ts
@@ -1,16 +1,16 @@
-// import { app, InvocationContext } from "@azure/functions";
-// import { TLadokEventUserProperties } from "./ladok-events/types";
+import { app, InvocationContext } from "@azure/functions";
+import { TLadokEventUserProperties } from "./ladok-events/types";
 
-// const SUBSCRIPTION_NAME = process.env.LADOK3_FEED_SERVICE_BUS_SUBSCRIPTION_NAME ?? "";
-// // const CONNECTION_STRING = process.env.LADOK3_FEED_SERVICE_BUS_CONNECTION_STRING ?? "" ;
+const SUBSCRIPTION_NAME = process.env.LADOK3_FEED_SERVICE_BUS_SUBSCRIPTION_NAME ?? "";
+// const CONNECTION_STRING = process.env.LADOK3_FEED_SERVICE_BUS_CONNECTION_STRING ?? "" ;
 
-// export async function LadokEvents(message: unknown, context: InvocationContext): Promise<void> {
-//   context.log('ladok3EventType:', (context?.triggerMetadata?.userProperties as TLadokEventUserProperties).ladok3EventType)
-// }
+export async function LadokEvents(message: unknown, context: InvocationContext): Promise<void> {
+  context.log('ladok3EventType:', (context?.triggerMetadata?.userProperties as TLadokEventUserProperties).ladok3EventType)
+}
 
-// app.serviceBusTopic('LadokEvents', {
-//   connection: 'LADOK3_FEED_SERVICE_BUS_CONNECTION_STRING',
-//   topicName: 'ladok3-feed',
-//   subscriptionName: SUBSCRIPTION_NAME,
-//   handler: LadokEvents
-// });
+app.serviceBusTopic('LadokEvents', {
+  connection: 'LADOK3_FEED_SERVICE_BUS_CONNECTION_STRING',
+  topicName: 'ladok3-feed',
+  subscriptionName: SUBSCRIPTION_NAME,
+  handler: LadokEvents
+});
