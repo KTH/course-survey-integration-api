3a74005e4638c699beb662969f0467473f92642a
Sebastian Ware <sebastian@urbantalk.se>
2023-12-21 17:36:03 +0100

    Fix tests and improve mocking of context to allow testing output bindings

--- __tests__/LadokEvents/utils/mockContext.ts
+++ __tests__/LadokEvents/utils/mockContext.ts
@@ -1,4 +1,5 @@
 import { InvocationContext } from "@azure/functions";
+import { TLadokEventUserProperties } from "../../../src/functions/ladok-events/types";
 export class MockContext implements InvocationContext {
   invocationId: any = undefined;
   functionName: any = undefined;
@@ -11,6 +12,7 @@ export class MockContext implements InvocationContext {
   error: any = undefined;
   options: any = undefined;
   
+  triggerMetadata: Record<string, unknown>;
   log: jest.Mock;
   done: jest.Mock;
   res: {
@@ -18,12 +20,61 @@ export class MockContext implements InvocationContext {
     body: jest.Mock;
   };
   
-  constructor() {
+  constructor(userProperties: TLadokEventUserProperties, appConfig?: Record<string, unknown>) {
+    this.triggerMetadata = {
+      userProperties,
+    };
+
+    if (appConfig) {
+      const { extraInputs = [], extraOutputs = [], ...rest } = appConfig;
+      this.extraInputs = new ExtraInputs(extraInputs);
+      this.extraOutputs = new ExtraOutputs(extraOutputs);      
+    }
+    
     this.log = jest.fn();
     this.done = jest.fn();
     this.res = {
       status: jest.fn(),
       body: jest.fn(),
     };
+  }
+}
+
+class ExtraOutputs {
+  _outputs: any[] = [];
+  _results: any[];
+  constructor(outputs: unknown = []) {
+    this._outputs = outputs as any[];
+    this._results = [];
+  }
+
+  /**
+   * Store the result of a function in the _result array
+   * @param outp 
+   * @param doc 
+   */
+  set(outp: any, doc: any) {
+    const index = this._outputs?.indexOf(outp);
+    this._results[index] = doc;
+  }
+
+  /**
+   * Retrieve the result of a function from the _result array using the output
+   * config object as key
+   * @param outp 
+   * @returns 
+   */
+  _getResult(outp: any) {
+    const index = this._outputs?.indexOf(outp);
+    return this._results[index];
+  }
+}
+
+class ExtraInputs {
+  _inputs: any[] = [];
+  _results: any[];
+  constructor(inputs: unknown = []) {
+    this._inputs = inputs as any[];
+    this._results = [];
   }
 }
