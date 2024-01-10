21b9ca7fea6a4d2593b8ea6e1a5d2e1210f40fb6
Sebastian Ware <sebastian@urbantalk.se>
2024-01-10 14:04:38 +0100

    Added mockDb to some tests

--- __tests__/LadokEvents/kurstillfalleTillStatusEvent.spec.ts
+++ __tests__/LadokEvents/kurstillfalleTillStatusEvent.spec.ts
@@ -1,25 +1,29 @@
 import event from "./fixtures/events/01_utbildningsinformation.KurstillfalleTillStatusEvent_1.json";
 import { handler } from "../../src/functions/ladok-events/kurstillfalleTillStatusEvent";
 import { MockContext } from "./utils/mockContext";
+import { MockDatabase } from "./utils/mockDatabase";
 
 describe("RegistreringEvent", () => {
 
   test("can be executed", async () => {
+    const mockDb = new MockDatabase();
     const mockContext = new MockContext(event.userProps);
-    await handler(event.message, mockContext);
+    await handler(event.message, mockContext, mockDb);
   });
 
   test("logs invocation", async () => {
+    const mockDb = new MockDatabase();
     const mockContext = new MockContext(event.userProps);
-    await handler(event.message, mockContext);
+    await handler(event.message, mockContext, mockDb);
 
     expect(mockContext.log.mock.calls.length).toBe(1);
     expect(mockContext.log.mock.lastCall[0]).toBe(`KurstillfalleTillStatusEvent: ${event.message.UtbildningstillfalleUID} ${event.message.Status}`);
   });
   
   test.skip("writes correct data to db", async () => {
+    const mockDb = new MockDatabase();
     const mockContext = new MockContext(event.userProps);
-    const outp = await handler(event.message, mockContext);
+    const outp = await handler(event.message, mockContext, mockDb);
     expect(outp).toMatchSnapshot();
   });
 });
--- __tests__/LadokEvents/modulTillStatusEvent.spec.ts
+++ __tests__/LadokEvents/modulTillStatusEvent.spec.ts
@@ -1,25 +1,29 @@
 import event from "./fixtures/events/01_utbildningsinformation.ModulTillStatusEvent_1.json";
 import { handler } from "../../src/functions/ladok-events/modulTillStatusEvent";
 import { MockContext } from "./utils/mockContext";
+import { MockDatabase } from "./utils/mockDatabase";
 
 describe("RegistreringEvent", () => {
 
   test("can be executed", async () => {
+    const mockDb = new MockDatabase();
     const mockContext = new MockContext(event.userProps);
-    await handler(event.message, mockContext);
+    await handler(event.message, mockContext, mockDb);
   });
 
   test("logs invocation", async () => {
+    const mockDb = new MockDatabase();
     const mockContext = new MockContext(event.userProps);
-    await handler(event.message, mockContext);
+    await handler(event.message, mockContext, mockDb);
 
     expect(mockContext.log.mock.calls.length).toBe(1);
     expect(mockContext.log.mock.lastCall[0]).toBe(`ModulTillStatusEvent: ${event.message.UtbildningsinstansUID} ${event.message.Status}`);
   });
   
   test.skip("writes correct data to db", async () => {
+    const mockDb = new MockDatabase();
     const mockContext = new MockContext(event.userProps);
-    const outp = await handler(event.message, mockContext);
+    const outp = await handler(event.message, mockContext, mockDb);
 
     expect(outp).toMatchSnapshot();
   });
