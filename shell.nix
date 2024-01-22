fc09eb8bee75ec1616d3e26f90949db7b48246d6
Sebastian Ware <sebastian@urbantalk.se>
2024-01-22 13:54:59 +0100

    Clarification

--- shell.nix
+++ shell.nix
@@ -1,3 +1,5 @@
+# This is a nix shell file that will install all the dependencies needed to run the webdev project
+# Instructions can be found in README.md.
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs {};
