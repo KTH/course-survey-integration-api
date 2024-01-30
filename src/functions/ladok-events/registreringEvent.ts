0047488924fb53e043c3b5effb82a4f42b45ae0a
Carlos Saito <carlos@saito.style>
2024-01-30 09:56:22 +0100

    Continue handling registreringEvent

--- src/functions/ladok-events/registreringEvent.ts
+++ src/functions/ladok-events/registreringEvent.ts
@@ -2,27 +2,33 @@ import { InvocationContext } from "@azure/functions";
 import { TLadokEventContext } from "./types";
 import { ServiceBus, isValidEvent, Database } from "../utils";
 import { TProgramRound, TStudentParticipation } from "../interface";
+import { getUgUser, getUgUserFromLadokId } from "ug-integration";
+import assert from "node:assert";
 
 export type TRegistreringEvent = {
-  Omfattningsvarde: string, // "10.0",
-  Studieavgiftsbetalning: string, // "studiedeltagande.domain.studieavgiftsbetalningsvarde.ej_aktuell",
-  StudieperiodSlut: string, // "2024-01-15",
-  StudieperiodStart: string, // "2023-08-28",
-  ArAnpassad: boolean, // false,
-  HandelseUID: string, // "8fb63aea-950a-11ee-99ff-6b3efc3c4159",
-  EventContext: TLadokEventContext,
-  KursUID: string, // "e51b9586-9501-11ee-a0ce-a9a57d284dbd",
-  KurstillfalleUID: string, // "41717c91-4028-11ee-bf53-2115569549a8",
-  Period: number, // 1,
-  RegistreringUID: string, // "64a2a94f-9509-11ee-99ff-6b3efc3c4159",
-  StudentUID: string, // "bbcce853-4df3-11e8-a562-6ec76bb54b9f",
-  TillfallesantagningUID: string, // "64a2a94e-9509-11ee-99ff-6b3efc3c4159",
-  UtbildningsinstansUID: string, // "e51b9585-9501-11ee-a0ce-a9a57d284dbd"
-}
+  Omfattningsvarde: string; // "10.0",
+  Studieavgiftsbetalning: string; // "studiedeltagande.domain.studieavgiftsbetalningsvarde.ej_aktuell",
+  StudieperiodSlut: string; // "2024-01-15",
+  StudieperiodStart: string; // "2023-08-28",
+  ArAnpassad: boolean; // false,
+  HandelseUID: string; // "8fb63aea-950a-11ee-99ff-6b3efc3c4159",
+  EventContext: TLadokEventContext;
+  KursUID: string; // "e51b9586-9501-11ee-a0ce-a9a57d284dbd",
+  KurstillfalleUID: string; // "41717c91-4028-11ee-bf53-2115569549a8",
+  Period: number; // 1,
+  RegistreringUID: string; // "64a2a94f-9509-11ee-99ff-6b3efc3c4159",
+  StudentUID: string; // "bbcce853-4df3-11e8-a562-6ec76bb54b9f",
+  TillfallesantagningUID: string; // "64a2a94e-9509-11ee-99ff-6b3efc3c4159",
+  UtbildningsinstansUID: string; // "e51b9585-9501-11ee-a0ce-a9a57d284dbd"
+};
 
-export async function handler(message: TRegistreringEvent, context: InvocationContext, db: Database): Promise<void> {
+export async function handler(
+  message: TRegistreringEvent,
+  context: InvocationContext,
+  db: Database,
+): Promise<void> {
   // if (!isValidEvent("se.ladok.schemas.studiedeltagande.RegistreringEvent", context?.triggerMetadata?.userProperties)) return;
-  
+
   // TODO: Consider using zod to validate the message
 
   const ladokCourseRoundId = message.KurstillfalleUID;
@@ -31,7 +37,10 @@ export async function handler(message: TRegistreringEvent, context: InvocationCo
   context.log(`RegistreringEvent: ${ladokCourseRoundId} ${ladokStudentId}`);
 
   const id = `${ladokStudentId}.${ladokCourseRoundId}`;
-  const studentParticipation: TStudentParticipation = await db.fetchById(id, "StudentParticipation");
+  const studentParticipation: TStudentParticipation = await db.fetchById(
+    id,
+    "StudentParticipation",
+  );
 
   if (studentParticipation) {
     // StudentParticipation exists
@@ -40,6 +49,8 @@ export async function handler(message: TRegistreringEvent, context: InvocationCo
   }
 
   // const studentInfo = ???;
+  const ugUser = await getUgUserFromLadokId(ladokStudentId);
+  assert(ugUser !== undefined, "");
 
   // 1. Create a StudentParticipation object
   const doc: TStudentParticipation = {
@@ -48,15 +59,15 @@ export async function handler(message: TRegistreringEvent, context: InvocationCo
     ladokCourseId,
     ladokCourseRoundId,
     /** @description We currently use kthUserId as canvasSisId */
-    canvasSisId: "TBD",
+    canvasSisId: ugUser.kthid,
     /** @description Full Name of user */
-    name: "TBD",
+    name: `${ugUser.givenName} ${ugUser.surname}`,
     /** Format: email */
-    email: "TBD",
+    email: ugUser?.email,
     roles: ["TBD"],
-    locations: ["TBD"],
-    program: {} as TProgramRound
-  }
+    locations: [],
+    program: {} as TProgramRound,
+  };
   // 2. Get more student info from UG REST API
   // 3. Persist in DB
   await db.insert(doc, "StudentParticipation");
@@ -68,4 +79,4 @@ export default {
   // input binding doesn't support cosmos document store yet
   // extraInputs: [cosmosInput],
   // extraOutputs: [cosmosOutput],
-}
+};
