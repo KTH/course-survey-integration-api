c5a2cfe5aaf38246a8f4f324571d01b6ae399c98
Carlos Saito <carlos@saito.style>
2024-02-05 13:47:34 +0100

    Delete a typ

--- packages/ladok-integration/src/api.ts
+++ packages/ladok-integration/src/api.ts
@@ -80,7 +80,6 @@ export async function getOrganisation(organisationUID: string) {
   );
 }
 
-//
 export async function getKurstillfallesdeltagande(studentUID: string) {
   return typedGet(
     `studiedeltagande/tillfallesdeltagande/kurstillfallesdeltagande/student/${studentUID}`,
