7b9a958bf91678014925806e081885a210825eb0
Carlos Saito <carlos@saito.style>
2024-02-05 11:11:08 +0100

    Functions to get terms

--- packages/ladok-integration/src/utils.ts
+++ packages/ladok-integration/src/utils.ts
@@ -1,5 +1,6 @@
 import type { z } from "zod";
 import { LadokOrganisation } from "./types";
+import assert from "node:assert";
 
 type LadokOrganisation = z.infer<typeof LadokOrganisation>;
 
@@ -76,4 +77,38 @@ export function parseOrganisation(organisation: LadokOrganisation) {
       },
     },
   };
+}
+
+/**
+ * Get the year and term from a date
+ * @param {string} date  Date formatted as `YYYY-MM-DD`
+ * @return a string "XXXXY" where XXXX is a year and Y is the term (1 for spring, 2 for autumn)
+ */
+export function getTermFromDate(date: string) {
+  const REGEX = /(\d\d\d\d)\-(\d\d)\-(\d\d)/;
+  const m = date.match(REGEX);
+
+  console.log(date, m);
+
+  assert(m !== null, "Error!!1");
+
+  const year = m[1];
+  const month = parseInt(m[2], 10);
+
+  if (month <= 6) {
+    return `${year}1`;
+  } else {
+    return `${year}2`;
+  }
+}
+
+/** Get the difference between two terms */
+export function diffTerms(t1: string, t2: string) {
+  // 1. Number of years between the periods
+  const yDiff = parseInt(t1.slice(0, 4), 10) - parseInt(t2.slice(0, 4), 10);
+
+  // 2. If they are not the same period, add or substract 1
+  const tDiff = parseInt(t1.slice(-1), 10) - parseInt(t2.slice(-1), 10);
+
+  return yDiff * 2 + tDiff;
 }
