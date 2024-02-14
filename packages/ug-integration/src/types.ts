d3c29204bfa365ce50e1266d21ec436e1468ed2d
Carlos Saito <carlos@saito.style>
2024-02-14 09:19:11 +0100

    Clean-up

--- packages/ug-integration/src/types.ts
+++ packages/ug-integration/src/types.ts
@@ -1,24 +1,5 @@
 import { z } from "zod";
 
-// Check response data from UG
-export function checkGetUgCourseResponsibleAndTeachers(val: Array<any>): void {
-  if (val.length !== 2) {
-    throw new Error(`Expected 2 values, got ${val.length}`);
-  }
-
-  if (val[0] === undefined) {
-    throw new Error(`Course responsible is undefined`);
-  }
-
-  if (val[0].startsWith("u1") === false) {
-    throw new Error(`Course responsible is not a kthid`);
-  }
-
-  if (val[1] === undefined) {
-    throw new Error(`Course teachers is undefined`);
-  }
-}
-
 export const UgUser = z.object({
   email: z.string(),
   kthid: z.string(),
@@ -28,13 +9,27 @@ export const UgUser = z.object({
 
 export const UgSchool = z
   .object({
-    name: z.string(),
-    kthid: z.string(),
+    name: z.string().min(1),
+    kthid: z.string().min(1),
     description: z
       .object({
-        sv: z.string(),
-        en: z.string(),
+        sv: z.string().optional(),
+        en: z.string().optional(),
       })
-      .partial(),
+      .required(),
   })
   .required();
+
+export const UgGroups = z.array(
+  z.object({
+    name: z.string().min(1),
+    kthid: z.string().min(1),
+    description: z
+      .object({
+        sv: z.string().optional(),
+        en: z.string().optional(),
+      })
+      .required(),
+    members: z.array(z.string()),
+  }),
+);
