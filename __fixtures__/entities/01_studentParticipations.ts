1004acdbbf4332045c91cbf2275b59933c06c64d
Carlos Saito <carlos@saito.style>
2024-02-22 13:09:14 +0100

    WIP

---
+++ __fixtures__/entities/01_studentParticipations.ts
@@ -0,0 +1,22 @@
+import { TStudentParticipationEntity } from "../../src/functions/interface";
+
+export const StudentParticipation2: TStudentParticipationEntity[] = [
+  {
+    id: "1",
+    parentId: "41374a41-2bda-404f-b11c-6e007deb61b4",
+    hashedStudentId: "0cd086ab-78ee-4b7c-a785-29821b364111",
+    ladokCourseRoundId: "41374a41-2bda-404f-b11c-6e007deb61b4",
+    canvasSisId: "u9sebbe0",
+    name: "Sebastian Sebsson",
+    email: "sebbe@example.com",
+    roles: ["student"],
+    program: {
+      code: "ARKIT",
+      name: "Arkitektur",
+      required: "mandatory?",
+      semester: "????",
+      startTerm: "20171",
+      studyYear: 4,
+    }
+  }
+]
