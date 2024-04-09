874ab247f42d17e766fa841fb407d99b936d678e
Sebastian Ware <sebastian@urbantalk.se>
2024-04-09 16:11:07 +0200

    Fix test

--- __integration__/LadokEvents/registreringEvent.integration.ts
+++ __integration__/LadokEvents/registreringEvent.integration.ts
@@ -3,7 +3,7 @@ import event2 from "../../__fixtures__/events/04_manuallycreated.RegistreringEve
 import handler from "../../src/functions/ladok-events/registreringEvent";
 import { MockContext } from "../../__tests__/utils/mockContext";
 import { MockDatabase } from "../../__tests__/utils/mockDatabase";
-import studentParticipation_01 from "../../__fixtures__/entities/01_studentParticipation.json";
+import { studentParticipations1 } from "../../__fixtures__/entities/01_studentParticipations";
 
 describe("RegistreringEvent INTEGRATION", () => {
   test("can be executed", async () => {
@@ -14,7 +14,7 @@ describe("RegistreringEvent INTEGRATION", () => {
 
   test("writes correct data to db", async () => {
     const mockDb = new MockDatabase({
-      StudentParticipation: studentParticipation_01,
+      StudentParticipation: studentParticipations1,
     });
     const mockContext = new MockContext(event1.userProps);
     await handler(event1.message, mockContext, mockDb);
