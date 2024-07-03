653fe1f5ed156987b1a0e07f59ce07e39b94bc1c
Sebastian Ware <sebastian@urbantalk.se>
2024-07-03 14:03:29 +0200

    Improve stacktraces

--- packages/ladok-integration/src/api.ts
+++ packages/ladok-integration/src/api.ts
@@ -53,57 +53,74 @@ function errorHandler(endpoint: string, error: unknown): never {
   throw error;
 }
 
-function typedGet<T>(endpoint: string, type: ZodSchema<T>): Promise<T> {
-  return gotClient
-    .get(endpoint)
-    .then((r) => type.parse(r.body))
+/*
+  Removed the typechecking function because the stacktrace became hard to read
+
+  ApiError [UnhandledApiError]: Response code 400 (Bad Request)
+    at errorHandler (/home/site/wwwroot/node_modules/ladok-integration/dist/api.js:58:15)
+    at /home/site/wwwroot/node_modules/ladok-integration/dist/api.js:69:25
+    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+    at async getCourseRoundInformation (/home/site/wwwroot/node_modules/ladok-integration/dist/index.js:18:27)
+    at async handler (/home/site/wwwroot/dist/src/functions/ladok-events/paborjatUtbildningstillfalleEvent.js:27:38)
+    at async /home/site/wwwroot/dist/src/functions/utils.js:44:20
+
+  The line dist/api.js:69:25 pointed to the general typechecking function
+*/
+
+export async function getCurrentUser() {
+  const endpoint = "kataloginformation/anvandare/autentiserad";
+  const zodType = LadokAnvandare;
+  return gotClient(endpoint)
+    .then((r) => zodType.parse(r.body))
     .catch((err) => errorHandler(endpoint, err));
 }
 
-export async function getCurrentUser() {
-  return typedGet("kataloginformation/anvandare/autentiserad", LadokAnvandare);
-}
-
 export async function getKurstillfalle(kurstillfalleUID: string) {
-  return typedGet(
-    `resultat/kurstillfalle/${kurstillfalleUID}/moment`,
-    LadokKurstillfalleMoment,
-  );
+  const endpoint = `resultat/kurstillfalle/${kurstillfalleUID}/moment`;
+  const zodType = LadokKurstillfalleMoment;
+  return gotClient(endpoint)
+    .then((r) => zodType.parse(r.body))
+    .catch((err) => errorHandler(endpoint, err));
 }
 
 export async function getKursinstans(utbildningsinstansUID: string) {
-  return typedGet(
-    `resultat/utbildningsinstans/kursinstans/${utbildningsinstansUID}`,
-    LadokKursinstans,
-  );
+  const endpoint = `resultat/utbildningsinstans/kursinstans/${utbildningsinstansUID}`;
+  const zodType = LadokKursinstans;
+  return gotClient(endpoint)
+    .then((r) => zodType.parse(r.body))
+    .catch((err) => errorHandler(endpoint, err));
 }
 
 export async function getOrganisation(organisationUID: string) {
-  return typedGet(
-    `kataloginformation/organisation/${organisationUID}`,
-    LadokOrganisation,
-  );
+  const endpoint = `kataloginformation/organisation/${organisationUID}`;
+  const zodType = LadokOrganisation;
+  return gotClient(endpoint)
+    .then((r) => zodType.parse(r.body))
+    .catch((err) => errorHandler(endpoint, err));
 }
 
 export async function getKurstillfallesdeltagande(studentUID: string) {
-  return typedGet(
-    `studiedeltagande/tillfallesdeltagande/kurstillfallesdeltagande/student/${studentUID}`,
-    Kurstillfallesdeltagande,
-  );
+  const endpoint = `studiedeltagande/tillfallesdeltagande/kurstillfallesdeltagande/student/${studentUID}`;
+  const zodType = Kurstillfallesdeltagande;
+  return gotClient(endpoint)
+    .then((r) => zodType.parse(r.body))
+    .catch((err) => errorHandler(endpoint, err));
 }
 
 export async function getStudiestruktur(studentUID: string) {
-  return typedGet(
-    `studiedeltagande/studiestruktur/student/${studentUID}`,
-    Studiestruktur,
-  );
+  const endpoint = `studiedeltagande/studiestruktur/student/${studentUID}`;
+  const zodType = Studiestruktur;
+  return gotClient(endpoint)
+    .then((r) => zodType.parse(r.body))
+    .catch((err) => errorHandler(endpoint, err));
 }
 
 export async function getUtbildningstillfalle(utbildningstillfalleUID: string) {
-  return typedGet(
-    `utbildningsinformation/utbildningstillfalle/${utbildningstillfalleUID}`,
-    Utbildningstillfalle,
-  );
+  const endpoint = `utbildningsinformation/utbildningstillfalle/${utbildningstillfalleUID}`;
+  const zodType = Utbildningstillfalle;
+  return gotClient(endpoint)
+    .then((r) => zodType.parse(r.body))
+    .catch((err) => errorHandler(endpoint, err));
 }
 
 // /utbildningsinformation/internal/utbildningstillfalle/647a7d3d-9a89-11ee-bfe5-0721067a4fbf
